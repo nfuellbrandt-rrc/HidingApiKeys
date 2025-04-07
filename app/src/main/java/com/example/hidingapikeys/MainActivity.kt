d3729ee54fbab94da5ac5d9af920dfd90bb6366d
Nicholas Fuellbrandt <nfuellbrandt@rrc.ca>
2025-04-07 18:29:40 -0500

    updated .gitignore to not include project files, why was it like that? who knows

---
+++ app/src/main/java/com/example/hidingapikeys/MainActivity.kt
@@ -0,0 +1,41 @@
+package com.example.hidingapikeys
+
+import android.os.Bundle
+import android.util.Log
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.hidingapikeys.ui.theme.HidingApiKeysTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            HidingApiKeysTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    val apiKey = BuildConfig.API_KEY
+                    Greeting(
+                        name = apiKey,
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = name,
+        modifier = modifier
+    )
+}
